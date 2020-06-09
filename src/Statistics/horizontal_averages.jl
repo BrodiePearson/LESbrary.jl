051009cf2694f213b5fd6058f26a3f661e36e172
Brodie Pearson <brodie.pearson@oregonstate.edu>
2020-06-08 17:23:46 -0700

    Add new basic diagnostics

--- src/Statistics/horizontal_averages.jl
+++ src/Statistics/horizontal_averages.jl
@@ -16,15 +16,40 @@ function horizontal_averages(model)
     # Extract short field names
     u, v, w = model.velocities
     U², V², W² = velocity_variances(model, scratch=scratch)
+    # Extract Pressure (I'm not sure this is available in model)
+    # p = model.pressure
 
     # Define horizontal averages
     U = HorizontalAverage(u)
     V = HorizontalAverage(v)
     e = TurbulentKineticEnergy(model)
+    # When pressure is available calculate it's layer mean
+    # P = HorizontalAverage(p)
 
     W³ = HorizontalAverage(w^3, scratch)
     wu = HorizontalAverage(w*u, scratch)
     wv = HorizontalAverage(w*v, scratch)
+    uv = HorizontalAverage((u-U)*(v-V), scratch)
+    wwu = HorizontalAverage(w*w*(u-U), scratch)
+    wwv = HorizontalAverage(w*w*(v-V), scratch)
+    wuu = HorizontalAverage(w*(u-U)*(u-U), scratch)
+    wvv = HorizontalAverage(w*(v-V)*(v-V), scratch)
+    wuv = HorizontalAverage(w*(u-U)*(v-V), scratch)
+    
+    # Diagnostics that involve pressure - need pressure passed
+    #wp = HorizontalAverage(w*(p-P), scratch)
+    #vp = HorizontalAverage((v-V)*(p-P), scratch)
+    #up = HorizontalAverage((u-U)*(p-P), scratch)
+    #pp = HorizontalAverage((p-P)*(p-P), scratch)
+    #p∂u∂x = HorizontalAverage((p-P)*∂x(u), scratch)
+    #p∂u∂y = HorizontalAverage((p-P)*∂y(u), scratch)
+    #p∂v∂z = HorizontalAverage((p-P)*∂z(u), scratch)
+    #p∂v∂x = HorizontalAverage((p-P)*∂x(v), scratch)
+    #p∂v∂y = HorizontalAverage((p-P)*∂y(v), scratch)
+    #p∂v∂z = HorizontalAverage((p-P)*∂z(v), scratch)
+    #p∂w∂x = HorizontalAverage((p-P)*∂x(w), scratch)
+    #p∂w∂y = HorizontalAverage((p-P)*∂y(w), scratch)
+    #p∂w∂z = HorizontalAverage((p-P)*∂z(w), scratch)
 
     primitive_averages = (
                   U = model -> U(model),
@@ -38,6 +63,29 @@ function horizontal_averages(model)
 
                  wu = model -> wu(model),
                  wv = model -> wv(model),
+                 uv = model -> uv(model),
+
+                 wwu = model -> wwu(model),
+                 wwv = model -> wwv(model),
+                 wuu = model -> wuu(model),
+                 wvv = model -> wvv(model),
+                 wuv = model -> wuv(model),
+
+                 #wp = model -> wu(model),
+                 #up = model -> wv(model),
+                 #vp = model -> uv(model),
+                 #pp = model -> wu(model),
+
+                 # Syntax doesn't seem to work for the following
+                 #p∂u∂x = model -> p∂u∂x(model),
+                 #p∂u∂y = model -> p∂u∂y(model),
+                 #p∂u∂z = model -> p∂u∂z(model),
+                 #p∂v∂x = model -> p∂v∂x(model),
+                 #p∂v∂y = model -> p∂v∂y(model),
+                 #p∂v∂z = model -> p∂v∂z(model),
+                 #p∂w∂x = model -> p∂w∂x(model),
+                 #p∂w∂y = model -> p∂w∂y(model),
+                 #p∂w∂z = model -> p∂w∂z(model),
                )
 
     # Add subfilter stresses (if they exist)
